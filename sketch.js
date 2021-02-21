387ad1b28335e8bbe86c601d0e60bae56f1bc8e4
whentheywanturname <75698798+whentheywanturname@users.noreply.github.com>
2021-02-21 12:44:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var dog,sadDog,happyDog;
+var bringtheFood, petliciousmenu
+var database; 
+  var whoneedsomemilk
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+  database = firebase.database();
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+}
+
+function draw() {
+  background(46,139,87);
+  drawSprites();
+}
+
+//function to read food Stock
+
+
+//function to update food stock and last fed time
+
+
+//function to add food in stock
